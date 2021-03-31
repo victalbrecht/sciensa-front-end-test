c88590c2e8a8b5e7f02c80bcdb9c871855d196a3
Victor Albrecht <victalbrecht@live.com>
2021-03-31 04:40:25 -0300

    100% responsive

--- src/app/domains/dashboard/animations/animations.ts
+++ src/app/domains/dashboard/animations/animations.ts
@@ -8,10 +8,10 @@ import {
 
 export const collapseAnimation: AnimationTriggerMetadata = trigger('collapse', [
   transition(':enter', [
-    style({ height: 0, overflow: 'hidden' }),
+    style({ height: 0, opacity: 0, overflow: 'hidden' }),
     animate('.3s ease-in-out'),
   ]),
   transition(':leave', [
-    animate('.3s ease-in-out', style({ height: 0, overflow: 'hidden' })),
+    animate('.3s ease-in-out', style({ height: 0, opacity: 0, overflow: 'hidden' })),
   ]),
 ]);
